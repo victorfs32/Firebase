5f4f4cc53052cc76e3869e210125dac4b6f25492
victorfs32 <jvfds66@gmail.com>
2023-05-16 13:32:29 -0300

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,17 +6,23 @@ import { FaFacebook } from "react-icons/fa";
 import { GoMarkGithub } from "react-icons/go";
 import { BiUserCircle } from "react-icons/bi";
 import { AiFillLock } from "react-icons/ai";
-import { HiOutlineMail } from 'react-icons/hi';
+import { HiOutlineMail } from "react-icons/hi";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "./Login.css"; // Importe o arquivo CSS
 
 function Login() {
+  const [isRegisterMode, setIsRegisterMode] = useState(false);
   const navigate = useNavigate();
   const [darkMode, setDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
+    setIsRegisterMode(false);
+  };
+
+  const toggleRegisterMode = () => {
+    setIsRegisterMode(!isRegisterMode);
   };
 
   const handleGitHubSignIn = () => {
@@ -64,28 +70,50 @@ function Login() {
     const email = event.target.elements.email.value;
     const password = event.target.elements.password.value;
 
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        // Usuário autenticado com sucesso
-        const user = result.user;
-        console.log(user);
-        alert("Você está conectado!");
-        setTimeout(() => {
-          navigate("/home"); // Redirecionamento após 4 segundos
-        }, 4000);
-      })
-      .catch((error) => {
-        // Trate os erros caso ocorra algum problema
-        console.error(error);
-      });
+    if (isRegisterMode) {
+      const username = event.target.elements.username.value;
+
+      // Registre o usuário com e-mail, senha e nome de usuário
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then((result) => {
+          // Usuário registrado com sucesso
+          const user = result.user;
+          console.log(user);
+          alert("Registro concluído! Você está conectado!");
+          setTimeout(() => {
+            navigate("/home"); // Redirecionamento após 4 segundos
+          }, 4000);
+        })
+        .catch((error) => {
+          // Trate os erros caso ocorra algum problema
+          console.error(error);
+        });
+    } else {
+      // Faça o login com e-mail e senha
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((result) => {
+          // Usuário autenticado com sucesso
+          const user = result.user;
+          console.log(user);
+          alert("Você está conectado!");
+          setTimeout(() => {
+            navigate("/home"); // Redirecionamento após 4 segundos
+          }, 4000);
+        })
+        .catch((error) => {
+          // Trate os erros caso ocorra algum problema
+          console.error(error);
+        });
+    }
   };
 
   const handlePasswordReset = (event) => {
     event.preventDefault();
     const email = event.target.elements.email.value;
-
     firebase
       .auth()
       .sendPasswordResetEmail(email)
@@ -101,10 +129,9 @@ function Login() {
     <main id="container" className={darkMode ? "dark-mode" : ""}>
       <form id="login_form" onSubmit={handleEmailSignIn}>
         <div id="form_header">
-          <h1>Login</h1>
+          <h1>{isRegisterMode ? "Register" : "Login"}</h1>
           <BsFillMoonFill id="mode_icon" onClick={toggleDarkMode} />
         </div>
-
         <div id="social_media">
           <a href="#">
             <FaFacebook id="img" />
@@ -117,6 +144,18 @@ function Login() {
           </a>
         </div>
         <div id="inputs">
+          {isRegisterMode && (
+            <div className="input-box">
+              <label htmlFor="username">
+                Username
+                <div className="input-field">
+                  <BiUserCircle id="user" />
+                  <input type="text" id="username" name="username" required />
+                </div>
+              </label>
+            </div>
+          )}
+
           <div className="input-box">
             <label htmlFor="email">
               E-mail
@@ -144,13 +183,18 @@ function Login() {
 
             <div id="forgot_password">
               <a href="#" onClick={handlePasswordReset}>
-                Forgot your password?
+                Redefinir minha senha?
+              </a>
+              <a id="Register" onClick={toggleRegisterMode}>
+                {isRegisterMode
+                  ? "Já tem uma conta? Login"
+                  : "Não tenho uma conta? Register"}
               </a>
             </div>
           </div>
         </div>
         <button type="submit" id="login_button">
-          Login
+          {isRegisterMode ? "Register" : "Login"}
         </button>
       </form>
     </main>
